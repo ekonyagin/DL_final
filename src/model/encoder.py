c0d0d2b18f0fe96d37592a7b7a09e40e6ea2a5e7
Leonid Litovchenko <litleo@mail.ru>
2020-06-02 02:00:34 +0300

    Add lighter version of HoloEncoder

--- src/model/encoder.py
+++ src/model/encoder.py
@@ -65,6 +65,10 @@ class ResBlock3d(nn.Module):
 
 
 class HoloEncoder(nn.Module):
+    '''
+        Encoder made in HoloGAN style.
+    '''
+
     def __init__(self, nf=32):
         super().__init__()
         self.nf = nf
@@ -138,6 +142,102 @@ class HoloEncoder(nn.Module):
 
         x = self.res_conv5(x)  # -> (bs, 512, 32, 32) NO DILATIONS, KS=3
         x = F.avg_pool2d(x, 2)  # -> (bs, 512, 16, 16)
+        x = x.reshape(bs, -1)
+
+        x = self.fc(x)  # -> (bs, 7 * 512)
+        latent_code = x.reshape(bs, 7, 512)
+
+        return latent_code
+
+
+class HoloEncoderLight(nn.Module):
+    '''
+        Lighter version of HoloEncoder.
+
+        Main difference is the downsampling part - it contains
+        one more conv resblock + adaptive_avg_pooling((8, 8))
+        and the number of channels decreases along the convs.
+
+    '''
+
+    def __init__(self, nf=32):
+        super().__init__()
+        self.nf = nf
+
+        self.conv1 = ResBlock2d(3, 128, kernel_size=3, dilation=2)
+        self.conv2 = ResBlock2d(128, self.nf * 128, kernel_size=3, dilation=2)
+
+        self.postproc = nn.Sequential(
+            nn.Conv3d(nf, nf // 2, kernel_size=3, padding=1),
+            nn.InstanceNorm3d(nf // 2, affine=True),
+            nn.ReLU(),
+            nn.Conv3d(nf // 2, nf // 4, kernel_size=3, padding=1),
+            nn.InstanceNorm3d(nf // 4, affine=True),
+            nn.ReLU(),
+        )
+
+        # nf=32 -> pnf=1024; nf=16 -> pnf=512; nf=8 -> pnf=256
+        pnf = (nf // 4) * 128  # 1024  #3d_channels * depth
+
+        # TODO: should be 1x1
+        self.proj = nn.Sequential(
+            nn.Conv2d(pnf, pnf // 2, kernel_size=3, padding=1),
+            nn.InstanceNorm2d(pnf // 2, affine=True),
+            nn.ReLU()
+        )
+
+        self.conv3 = ResBlock2d(pnf // 2, pnf // 4)
+        self.conv4 = ResBlock2d(pnf // 4, pnf // 8)
+        self.conv5 = ResBlock2d(pnf // 8, pnf // 16)
+        self.conv6 = ResBlock2d(pnf // 16, pnf // 32)
+
+        self.fc = nn.Sequential(
+            nn.Linear(pnf // 32 * 8 * 8, 7 * 512)
+        )
+
+    def stn(self, x, theta):
+        # theta must be (Bs, 3, 4) = [R|t]
+        # theta = theta.view(-1, 2, 3)
+
+        grid = F.affine_grid(theta, x.size())
+        out = F.grid_sample(x, grid, padding_mode='zeros')
+        return out
+
+    def forward(self, x, thetas):
+        '''
+            x: batch of images of shape (Bs, c, h, w)
+            thetas : Transform matrix, must be (Bs, 3, 4) = [R|t]
+        '''
+        bs = x.size(0)
+        x = self.conv1(x)  # -> (bs, 128, 128, 128)
+        x = self.conv2(x)  # -> (bs, 128*nf, 128, 128)
+
+        # reshape to (bs, 3d_channels, d, h, w)
+        x = x.reshape(bs, self.nf, x.size(1) // self.nf, x.size(2), x.size(3))
+
+        # Perform rotation
+        x = self.stn(x, thetas)
+
+        # Postprocess before projection
+        x = self.postproc(x)  # -> (bs, nf//4, 128, 128, 128)
+
+        # Projection unit. Concat depth and channels
+        x = x.reshape(bs, x.size(1) * x.size(2), x.size(3), x.size(4))
+        x = self.proj(x)  # -> (bs, 1024, 128, 128)
+
+        # Downsaple to (bs, 7 * 512)
+        x = self.conv3(x)  # -> (bs, 512, 128, 128) NO DILATIONS, KS=3
+        x = F.avg_pool2d(x, 2)  # -> (bs, 512, 64, 64)
+
+        x = self.conv4(x)  # -> (bs, 256, 64, 64) NO DILATIONS, KS=3
+        x = F.avg_pool2d(x, 2)  # -> (bs, 256, 32, 32)
+
+        x = self.conv5(x)  # -> (bs, 128, 32, 32) NO DILATIONS, KS=3
+        x = F.avg_pool2d(x, 2)  # -> (bs, 128, 16, 16)
+
+        x = self.conv6(x)  # -> (bs, 64, 32, 32) NO DILATIONS, KS=3
+        # x = F.avg_pool2d(x, 2) # -> (bs, 64, 8, 8)
+        x = F.adaptive_avg_pool2d(x, (8, 8))
         x = x.reshape(bs, -1)
 
         x = self.fc(x)  # -> (bs, 7 * 512)
